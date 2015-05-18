22302e91960a4d484c0b920a224cd9a7fadc003e
HairyBeast <jordanwoodage@gmail.com>
2015-05-18 15:57:36 +0100

    First Commit
    Cube unfolded, need to implement properly and work out a way of moving
    across the grid

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+
+$(document).ready(function(){
+
+
+	var count = 0;
+	var winWidth = $(window).width();
+	var winHeight = $(window).height();
+	
+	$(document).mousemove(function(e){
+      
+		var x = e.pageX;
+		var y = e.pageY;
+		
+		var rotateY = (360/winWidth) * y;
+		var rotateX = (360/winHeight) * x; 
+        if(count===0) {
+		
+		$('.cube').css({'transform':'rotateY(' + rotateX + 'deg) rotateX(' + rotateY + 'deg)'});
+	} 
+  
+    });
+    
+  
+  $( ".cube" ).click(function() {
+
+$('#s6 .top, #s6 .left,#s6 .right,#s6 .bottom,#s6 .back,#s6 .front').addClass('flat'); 
+count += 1;
+//     
+      $('.cube').css({'transform':'rotateY(' + '0' + 'deg) rotateX(' + '0' + 'deg)','transition':' all 3.3s ease'});
+
+    } );
+
+//   $(window).dblclick(function(){    
+// $('.cube, .cube .side').animate({
+//   width:"15em",
+//   height:"15em"
+// }, 500, function(){alert("oo");});
+
+//     $('.cube .top.flat').animate({
+//   top:"-14.75em"
+// }, 500, function(){alert("oo");});
+//     $('.cube .left.flat').animate({
+//   left:"-14.75em"
+// }, 500, function(){alert("oo");});
+//     $('.cube .right.flat').animate({
+//   left:"14.75em"
+// }, 500, function(){alert("oo");});
+//     $('.cube .bottom.flat').animate({
+//   top:"14.75em"
+// }, 500, function(){alert("oo");});
+//         $('.cube .back.flat').animate({
+//   top:"14.75em"
+// }, 500, function(){alert("oo");});
+// });
+      
+  console.log(count);
+});
+ 
+   
+ 
